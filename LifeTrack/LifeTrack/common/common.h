9b434bf4797c0da854ffa8ab8767cc26eb33bea2
inMiss <pandacreater@163.com>
2025-09-20 18:13:43 +0800

    Create common.h

---
+++ LifeTrack/LifeTrack/common/common.h
@@ -0,0 +1,20 @@
+#ifndef LIFETRACK_COMMON_COMMON_H
+#define LIFETRACK_COMMON_COMMON_H
+
+enum ELanguageType
+{
+    e_language_chinese = 0,
+    e_language_english
+};
+
+enum EPageID
+{
+    PID_MAINPAGE_ID = 100,
+    PID_TSAKPAGE_ID = 200,
+    PID_GOALPAGE_ID = 300,
+    PID_ANALYSISPAGE_ID = 400,
+    PID_SYSTEMPAGE_ID = 500
+};
+
+
+#endif
